4c7bf7d4bd322c4907c34fc7c15dab6cd1d47870
Abed Alrahman Al Najjar <151450832+Abed-Najjar@users.noreply.github.com>
2023-12-24 16:06:43 +0300

    Added Client,ClientTypes,Product,Order models

--- reservation/clients/admin.py
+++ reservation/clients/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Client,ClientTypes,Product,Order
 
 # Register your models here.
+
+admin.site.register(Client)
+admin.site.register(ClientTypes)
+admin.site.register(Product)
+admin.site.register(Order)
+
